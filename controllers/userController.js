00128bfbec3570941016f3d48e2c7efc01d4cb36
ziho <ziho@kakao.com>
2021-04-02 22:05:23 +0900

    Add signup api

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const bcrpyt = require("bcrypt");
+const User = require("../models/user");
+const statusCode = require("../module/statusCode");
+const jwt = require("../module/jwt");
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilio = require("twilio")(accountSid, authToken);
+
+module.exports = {
+	signup: async (req, res) => {
+		console.log(req.body);
+		const { email, nick, password, phone, gender, birth } = req.body;
+
+		try {
+			const exUser = await User.findOne({ where: { email } });
+			//이미 있는 아이디
+			if (exUser) {
+				return await res
+					.status(statusCode.BAD_REQUEST)
+					.send({ success: false, message: "이미 사용중인 이메일입니다." });
+			}
+			// 비밀번호 hash화, 12는 해쉬 난이도
+			const hashedPassword = await bcrpyt.hash(password, 12);
+			await User.create({
+				email: email,
+				nick,
+				password: hashedPassword,
+				phone,
+				gender,
+				birth,
+			});
+			return await res
+				.status(statusCode.OK)
+				.send({ success: true, message: "회원가입에 성공하였습니다", email });
+		} catch (err) {
+			console.log(err);
+			await res
+				.status(statusCode.INTERNAL_SERVER_ERROR)
+				.send({ success: false, message: "서버 내부 오류입니다." });
+		}
+	},
+};
