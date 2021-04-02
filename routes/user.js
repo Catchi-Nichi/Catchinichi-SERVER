b7b7b8f025508ef9a3ece5784529ff6c5ae5ff7d
ziho <ziho@kakao.com>
2021-04-02 22:07:21 +0900

    update user route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const { UserController } = require("../controllers/userController");
-//로그인 안한 사람만 회원가입 가능하게 하기 위해 미들웨어를 넣어줌
-router.post("/signup", isNotLoggedIn, UserController.signup);
+const UserController = require("../controllers/userController");
 
-router.post("/login", isNotLoggedIn, UserController.login);
-
-router.get("/logout", isLoggedIn, UserController.logout);
-
+router.post("/signup", UserController.signup);
+// router.post("/signin", UserController.signin); //로그인
+router.post("/checkEmail", UserController.checkEmail); //아아디 중복체크
+router.post("/checkNick", UserController.checkNickName); //닉네임 중복체크
+router.post("/verifyPhone", UserController.verifyPhone); // 휴대폰 인증
+// router.get("/profile", middleware.userJwt, UserController.profile); //프로필 조회
+// router.get("/myRoom", middleware.userJwt, UserController.myReviw); // 나의 모임
+// router.post("/updateToken", middleware.userJwt, UserController.updateToken); // 디바이스 토큰 업데이트
+// router.get("/checkDeviceToken", UserController.checkDeviceToken);
 module.exports = router;
