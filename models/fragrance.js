7694c1ebd889bc5186a5d4d659d9a95020eaa0dc
ziho <ziho@kakao.com>
2021-04-23 15:44:05 +0900

    add fragrance model

---
+++ models/fragrance.js
@@ -0,0 +1,49 @@
+const Sequelize = require("sequelize");
+
+module.exports = class Fragrance extends Sequelize.Model {
+	static init(sequelize) {
+		return super.init(
+			{
+				brand: {
+					type: Sequelize.STRING(50),
+					primaryKey: true,
+				},
+				kr_brand: {
+					type: Sequelize.STRING(20),
+				},
+				en_name: {
+					type: Sequelize.STRING(100),
+					primaryKey: true,
+				},
+				kr_name: {
+					type: Sequelize.STRING(45),
+				},
+				img: {
+					type: Sequelize.STRING(255),
+				},
+				category: {
+					type: Sequelize.ENUM("all", "nichi"),
+				},
+				likes: {
+					type: Sequelize.INTEGER,
+				},
+			},
+			{
+				sequelize,
+				timestamps: false,
+				underscored: false,
+				modelName: "Fragrance",
+				tableName: "fragrances",
+				//DeletedAt
+				paranoid: false,
+				//한글 지원
+				charset: "utf8",
+				collate: "utf8_general_ci",
+			}
+		);
+	}
+
+	// static associate(db) {
+	// 	db.User.hasMany(db.Review);
+	// }
+};
--- models/index.js
+++ models/index.js
@@ -4,14 +4,17 @@ const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
 
 const User = require("./user");
+const Fragrance = require("./fragrance");
+
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
 db.User = User;
+db.Fragrance = Fragrance;
 
 User.init(sequelize);
-
+Fragrance.init(sequelize);
 // User.associate(db);
-
+// Fragrance.associate(db);
 module.exports = db;
