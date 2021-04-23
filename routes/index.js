700452da9b04e437507e04269dff5ae95e81f8b4
ziho <ziho@kakao.com>
2021-04-23 15:54:35 +0900

    update fragrance and search router in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 
 router.use("/user", require("./user"));
 router.use("/auth", require("./token"));
+router.use("/fragrance", require("./fragrance"));
+router.use("/search", require("./search"));
 
 module.exports = router;
