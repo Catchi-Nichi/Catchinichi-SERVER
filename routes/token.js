a3318c14224c793fcb63d8db75ba0b6e6860a647
ziho <ziho@kakao.com>
2021-04-23 15:43:16 +0900

    add token api comment

--- routes/token.js
+++ routes/token.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userJwt = require("../module/middlewares");
 
-router.get("/request", userJwt.refreshToken);
+router.get("/request", userJwt.refreshToken); //액세스토큰 재발급
 
 module.exports = router;
