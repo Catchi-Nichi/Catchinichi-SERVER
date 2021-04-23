dfaff681200059edc1dffa46d62269baca9357cf
ziho <ziho@kakao.com>
2021-04-23 15:54:57 +0900

    add search controller

---
+++ controllers/searchController.js
@@ -0,0 +1,29 @@
+const Fragrance = require("../models/fragrance");
+const statusCode = require("../module/statusCode");
+const { QueryTypes } = require("sequelize");
+const { sequelize } = require("../models");
+module.exports = {
+	search: async (req, res) => {
+		let searchText = req.params.searchText;
+		searchText = `%${searchText.replace(" ", "%")}%`;
+		const query =
+			'select * from fragrances where replace(kr_brand," ","") like :searchText or replace(brand," ","") like :searchText or replace(kr_name," ","") like :searchText or replace(en_name," ","") like :searchText';
+		try {
+			const searchList = await sequelize.query(query, {
+				replacements: { searchText: searchText },
+				type: QueryTypes.SELECT,
+				raw: true,
+			});
+
+			const countingList = searchList.length;
+			res
+				.status(statusCode.OK)
+				.send({ success: true, message: `${countingList}개의 향수가 검색되었습니다.`, searchList });
+		} catch (err) {
+			console.log(err);
+			return res
+				.status(statusCode.INTERNAL_SERVER_ERROR)
+				.send({ success: false, message: "검색 중 에러가 발생하였습니다." });
+		}
+	},
+};
