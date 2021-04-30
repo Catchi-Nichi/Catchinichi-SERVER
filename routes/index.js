5452bbc37db283cfd70393fb75ec975ecbec35e6
ziho <ziho@kakao.com>
2021-04-30 10:24:11 +0900

    add / get api

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+	return res.status(200).send({ message: "main" });
+});
 router.use("/user", require("./user"));
 router.use("/auth", require("./token"));
 router.use("/fragrance", require("./fragrance"));
