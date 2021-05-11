caa85839091a0b709472ac7195d852cac33f69b8
ziho <ziho@kakao.com>
2021-05-11 14:04:59 +0900

    [update] 파일 삭제 주석 해제 ( 안드로이드 테스트 완료 )

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -123,9 +123,9 @@ module.exports = {
 						return db;
 					});
 					const searchList = await Promise.all(result);
-					// fs.unlink(filename, (err) => {
-					// 	if (err) console.log(err);
-					// });
+					fs.unlink(filename, (err) => {
+						if (err) console.log(err);
+					});
 					res.status(statusCode.OK).send({
 						success: true,
 						message: `향수가 검색되었습니다.`,
