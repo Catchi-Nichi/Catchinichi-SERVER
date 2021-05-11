26601a70bebd5e64fb8b7f0b736773593e483b9a
ziho <ziho@kakao.com>
2021-05-11 13:17:25 +0900

    [update] base64 length를 위해 express body limit 수정

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ sequelize
 //middleware
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: false, limit: "25mb" }));
 app.use(cookieParser());
 app.use(
 	cors({
