640dc7aa06c47dc3cd2da642a5c02c830466dcd8
ziho <ziho@kakao.com>
2021-05-21 13:51:51 +0900

    [add] 시향노트 API 컨트롤러 생성

---
+++ controllers/memoController.js
@@ -0,0 +1,106 @@
+const statusCode = require("../module/statusCode");
+const Memo = require("../models/memo");
+const Fragrance = require("../models/fragrance");
+module.exports = {
+	addMemo: async (req, res) => {
+		const { nick, brand, fragrance, comment } = req.body;
+		try {
+			await Memo.create({
+				nick,
+				kr_brand: brand,
+				kr_name: fragrance,
+				comment,
+			});
+			return res
+				.status(statusCode.OK)
+				.send({ success: true, message: "시향노트가 성공적으로 등록되었습니다." });
+		} catch (err) {
+			console.log(err);
+			return res
+				.status(statusCode.INTERNAL_SERVER_ERROR)
+				.send({ success: false, message: "서버 내부 오류입니다." });
+		}
+	},
+	updateMemo: async (req, res) => {
+		const idx = req.params.id;
+		const { comment } = req.body;
+		try {
+			await Memo.update(
+				{
+					comment,
+				},
+				{ where: { id: idx } }
+			);
+			return res
+				.status(statusCode.OK)
+				.send({ success: true, message: "시향노트가 성공적으로 수정되었습니다." });
+		} catch (err) {
+			console.log(err);
+			return res
+				.status(statusCode.INTERNAL_SERVER_ERROR)
+				.send({ success: false, message: "서버 내부 오류입니다." });
+		}
+	},
+	deleteMemo: async (req, res) => {
+		const idx = req.params.id;
+
+		try {
+			await Memo.destroy({
+				where: {
+					id: idx,
+				},
+			});
+			return res.status(statusCode.OK).send({
+				success: true,
+				message: "해당 시향노트가 삭제되었습니다.",
+			});
+		} catch (err) {
+			console.log(err);
+			return res
+				.status(statusCode.INTERNAL_SERVER_ERROR)
+				.send({ success: false, message: "서버 내부 오류입니다." });
+		}
+	},
+	loadAllMemo: async (req, res) => {
+		const { nick } = req.params;
+		try {
+			const memoList = await Memo.findAll({
+				attributes: ["id", "kr_brand", "kr_name", "comment", "updatedAt"],
+				include: [{ model: Fragrance, attributes: ["img", "likes", "avgStars"] }],
+				where: { nick },
+			});
+			console.log(memoList);
+			return res.status(statusCode.OK).send({
+				success: true,
+				message: "해당 사용자의 시향노트를 불러왔습니다.",
+				memoList,
+			});
+		} catch (err) {
+			console.log(err);
+			return res
+				.status(statusCode.INTERNAL_SERVER_ERROR)
+				.send({ success: false, message: "서버 내부 오류입니다." });
+		}
+	},
+	loadOneMemo: async (req, res) => {
+		const idx = req.params.id;
+		try {
+			const memoList = await Memo.findOne({
+				attributes: ["id", "kr_brand", "kr_name", "comment", "updatedAt"],
+				include: [{ model: Fragrance, attributes: ["img", "likes", "avgStars"] }],
+				where: { id: idx },
+			});
+			console.log(memoList);
+			return res.status(statusCode.OK).send({
+				success: true,
+				message: "해당 시향노트를 불러왔습니다.",
+				memoList,
+			});
+		} catch (err) {
+			console.log(err);
+			return res
+				.status(statusCode.INTERNAL_SERVER_ERROR)
+				.send({ success: false, message: "서버 내부 오류입니다." });
+		}
+	},
+};
