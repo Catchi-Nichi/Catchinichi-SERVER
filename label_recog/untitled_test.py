20fd2bb5a834bf33984e2dd77885abfa6a500048
Yujee Song <rswfaf@gmail.com>
2021-06-02 00:21:24 +0900

    more generality & test cases

---
+++ label_recog/untitled_test.py
@@ -0,0 +1,21 @@
+import untitled0
+import cv2
+import os
+
+
+def test_u0():
+    base_dir = "../test_failed"
+    img = os.listdir(base_dir)
+    for i in img:
+        img_dir = os.path.join(base_dir, i)
+        name = untitled0.main(img_dir)
+        print(i, name)
+
+        # image = cv2.imread(img_dir)
+        # image = cv2.resize(image, dsize=(0, 0), fx=0.3, fy=0.3)
+        # cv2.imshow(name, image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+test_u0()
