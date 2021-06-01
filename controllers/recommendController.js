6292182b0aa03737df7bb7540ce0bb8a33810f4d
ziho <ziho@kakao.com>
2021-06-01 09:10:21 +0900

    [update] 노트 기반 향수 추천 조합이 없을 경우 예외 처리

--- controllers/recommendController.js
+++ controllers/recommendController.js
@@ -9,13 +9,22 @@ module.exports = {
 	moodRecommend: async (req, res) => {
 		const { category1, category2 } = req.query;
 		const SQL_MOOD_RECOMMEND_QUERY = `SELECT distinct S.* FROM catchiNichi.notes as T,catchiNichi.notes as B,catchiNichi.fragrances as S where T.category = :category1 and B.category = :category2 and T.name = B.name and T.name = S.en_name limit 5;`;
-
+		const SQL_SECOND_MOOD_RECOMMEND_QUERY = `SELECT distinct S.* FROM catchiNichi.notes as T, catchiNichi.fragrances as S where T.category = :category1 and T.name = S.en_name order by S.countingReview DESC limit 5;`;
 		try {
-			const recommendList = await sequelize.query(SQL_MOOD_RECOMMEND_QUERY, {
+			let recommendList = await sequelize.query(SQL_MOOD_RECOMMEND_QUERY, {
 				replacements: { category1, category2 },
 				type: QueryTypes.SELECT,
 				raw: true,
 			});
+
+			if (recommendList.length < 1) {
+				recommendList = await sequelize.query(SQL_SECOND_MOOD_RECOMMEND_QUERY, {
+					replacements: { category1 },
+					type: QueryTypes.SELECT,
+					raw: true,
+				});
+			}
+
 			return res.status(statusCode.OK).send({
 				success: true,
 				message: `추천 향수를 불러왔습니다.`,
